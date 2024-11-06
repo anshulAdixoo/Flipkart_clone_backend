695c4ca9cd10b762eb364a2a1c7ed9b74d284b34
anshumanadixoo <Anshuman.Mishra@Adixoo.com>
2024-11-06 18:22:33 +0530

    created the user schema for users

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,12 @@
+// src/users/user.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { User } from './user.entity';
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController],
+  imports: [TypeOrmModule.forFeature([User])], // Register the User entity with TypeORM
+  providers: [UserService], // Provide the UserService for dependency injection
+  exports: [UserService], // Export UserService so it can be used in other modules (e.g., AuthModule)
 })
 export class UserModule {}
